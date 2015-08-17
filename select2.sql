42a54a8a36adb9aff98c8b7ba85508eddb5db428
scq000 <scq000@hotmail.com>
2015-08-17 22:28:16 +0800

    stastic female and male numbers

--- select2.sql
+++ select2.sql
@@ -2,4 +2,7 @@ USE TWDemo;
 
 SELECT student.name AS "学生姓名",course.name AS "所选课程" from student,course,student_course where student_id= student.id and course_id = course.id;
 
-SELECT grade.name AS "班级",COUNT(grade_id) AS "人数" FROM student,grade WHERE grade_id = grade.id GROUP BY grade_id;
+SELECT grade.name AS "班级",COUNT(grade_id) AS "人数",COUNT(CASE WHEN student.sex="female" THEN student.sex END) AS "男生",COUNT(CASE WHEN student.sex='male' THEN student.sex END) AS '女生' FROM student,grade WHERE grade_id = grade.id GROUP BY grade_id;
+
+
+SELECT sex as '性别',COUNT(id) AS '人数' FROM student GROUP BY sex;
