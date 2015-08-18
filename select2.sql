3485f48c28c91e80b4de4ecfe93c7bfb86a9dd03
scq000 <scq000@hotmail.com>
2015-08-18 10:24:58 +0800

    optimize the search sql

--- select2.sql
+++ select2.sql
@@ -1,8 +1,8 @@
 USE TWDemo;
 
-SELECT student.name AS "学生姓名",course.name AS "所选课程" from student,course,student_course where student_id= student.id and course_id = course.id;
+SELECT student.name AS '学生姓名',group_concat(course.name) AS '所选课程' FROM student,course,student_course WHERE student_id=student.id AND course_id=course.id GROUP BY student_id;
 
-SELECT grade.name AS "班级",COUNT(grade_id) AS "人数",COUNT(CASE WHEN student.sex="female" THEN student.sex END) AS "男生",COUNT(CASE WHEN student.sex='male' THEN student.sex END) AS '女生' FROM student,grade WHERE grade_id = grade.id GROUP BY grade_id;
+SELECT grade.name AS "班级",COUNT(grade_id) AS "人数", SUM(student.sex='female') AS "男生",SUM(student.sex='male') AS '女生' FROM student,grade WHERE grade_id = grade.id GROUP BY grade_id;
 
 
 SELECT sex as '性别',COUNT(id) AS '人数' FROM student GROUP BY sex;
